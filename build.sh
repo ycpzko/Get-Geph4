2d26badde4692057117fcf6dbce92e521575248b
鷺宮しおり <69150021+SaginomiyaShiori@users.noreply.github.com>
2020-12-25 13:27:00 +0800

    all build

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+rustup default nightly
+cargo install cross
+export PATH=~/.cargo/bin:$PATH
+git clone https://github.com/geph-official/geph4
+cd geph4
+
+sh osx_build.sh
+
+cross build --release --locked --target i686-pc-windows-gnu --manifest-path=geph4-client/Cargo.toml
+cross build --release --locked --target x86_64-unknown-linux-musl --manifest-path=geph4-client/Cargo.toml
+cross build --release --locked --target x86_64-unknown-linux-gnu --manifest-path=geph4-vpn-helper/Cargo.toml
+cross build --release --locked --target armv7-linux-androideabi --manifest-path=geph4-client/Cargo.toml
+cross build --release --locked --target aarch64-linux-android --manifest-path=geph4-client/Cargo.toml
+cross build --release --locked --target armv7-unknown-linux-musleabihf --manifest-path=geph4-client/Cargo.toml
+cross build --release --locked --target aarch64-unknown-linux-musl --manifest-path=geph4-client/Cargo.toml
+mkdir OUTPUT
+mv target/x86_64-unknown-linux-musl/release/geph4-client OUTPUT/geph4-client-linux-amd64
+mv target/x86_64-unknown-linux-gnu/release/geph4-vpn-helper OUTPUT/geph4-vpn-helper-linux-amd64
+mv target/armv7-unknown-linux-musleabihf/release/geph4-client OUTPUT/geph4-client-linux-armv7
+mv target/armv7-linux-androideabi/release/geph4-client OUTPUT/geph4-client-android-armv7
+mv target/aarch64-linux-android/release/geph4-client OUTPUT/geph4-client-android-aarch64
+mv target/i686-pc-windows-gnu/release/geph4-client.exe OUTPUT/geph4-client-windows-i386.exe
+mv target/x86_64-apple-darwin/release/geph4-client OUTPUT/geph4-client-macos-amd64
+mv target/aarch64-unknown-linux-musl/release/geph4-client OUTPUT/geph4-client-linux-aarch64
+exit 0
---
+++ osx_build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+sudo apt install \
+    clang \
+    gcc \
+    g++ \
+    zlib1g-dev \
+    libmpc-dev \
+    libmpfr-dev \
+    libgmp-dev \
+    build-essential
+
+# Add macOS Rust target
+rustup target add x86_64-apple-darwin
+
+git clone https://github.com/tpoechtrager/osxcross
+cd osxcross
+wget -nc https://s3.dockerproject.org/darwin/v2/MacOSX10.10.sdk.tar.xz
+mv MacOSX10.10.sdk.tar.xz tarballs/
+UNATTENDED=yes OSX_VERSION_MIN=10.7 ./build.sh
+PATH="$(pwd)/target/bin:$PATH" \
+CC=o64-clang \
+CXX=o64-clang++ \
+cargo build --release  --locked --target x86_64-apple-darwin --manifest-path=../geph4-client/Cargo.toml
