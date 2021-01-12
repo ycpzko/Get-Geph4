fbe5d9cc62c7ebb69ee34b06e79162dd187644bf
鷺宮しおり <69150021+SaginomiyaShiori@users.noreply.github.com>
2021-01-13 04:55:08 +0800

    update to 4.0.6

--- get-geph4.py
+++ get-geph4.py
@@ -3,9 +3,6 @@ import urllib.request
 import os
 saveName='Cargo.toml'
 localFile='geph4Version'
-def delfile():
-    if os.path.exists(saveName):
-        os.remove(saveName)
 # Download Cargo.toml
 gephTomlUrl='https://github.com/geph-official/geph4/raw/master/geph4-client/Cargo.toml'
 urllib.request.urlretrieve(gephTomlUrl,saveName)
@@ -15,19 +12,18 @@ cargoToml=toml.loads(readFile.read())
 geph4Version=cargoToml['package']['version']
 print(geph4Version)
 readFile.close()
+os.remove(saveName)
 # Check Version
 if os.path.exists(localFile):
     localVer=open(localFile)
     if localVer.read() == geph4Version:
         print("Already the latest version")
         localVer.close()
-        delfile()
     else:
     # Write geph4Version
         f=open(localFile,'w')
         f.write(geph4Version)
         f.close()
-        delfile()
         # Git commit
         os.system("git add -A && git commit -am 'update to %s'" %geph4Version)
         os.system("git tag v%s && git push && git push origin --tags" %geph4Version)
