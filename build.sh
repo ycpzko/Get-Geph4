2f93b8faf7cd4438dc7cebd87e830ecc3dd390d9
showgood163 <blucmt@gmail.com>
2021-03-08 01:39:50 +0800

    Update build.sh
    Only keeps aarch64-unknown-linux-gnu build

--- build.sh
+++ build.sh
@@ -6,12 +6,7 @@ git clone https://github.com/geph-official/geph4
 cd geph4
 
 sh osx_build.sh
-
-cross build --release --locked --target x86_64-pc-linux-gnu --manifest-path=geph4-vpn-helper/Cargo.toml
-cross build --release --locked --target aarch64-linux-android --manifest-path=geph4-client/Cargo.toml
 cross build --release --locked --target aarch64-unknown-linux-gnu --manifest-path=geph4-client/Cargo.toml
 mkdir OUTPUT
-mv target/x86_64-pc-linux-gnu/release/geph4-client OUTPUT/geph4-client-linux-amd64
-mv target/aarch64-linux-android/release/geph4-client OUTPUT/geph4-client-android-aarch64
 mv target/aarch64-unknown-linux-gnu/release/geph4-client OUTPUT/geph4-client-linux-aarch64
 exit 0
