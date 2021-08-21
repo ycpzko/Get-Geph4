0d0a84ddccbfb81f38b6e979dd97163abb19d82d
ycpzko <50710126+ycpzko@users.noreply.github.com>
2021-08-21 21:30:15 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ git clone https://github.com/geph-official/geph4
 cd geph4
 
 sh osx_build.sh
-cross build --release --locked --target aarch64-unknown-linux-gnu --manifest-path=geph4-client/Cargo.toml
+cross build --release --locked --target aarch64-linux-android --manifest-path=geph4-client/Cargo.toml
 mkdir OUTPUT
-mv target/aarch64-unknown-linux-gnu/release/geph4-client OUTPUT/geph4-client-linux-aarch64
+mv target/aarch64-linux-android/release/geph4-client OUTPUT/geph4-client-android-aarch64
 exit 0
